10fb0992767f1133a8b2778263111b50db5c1153
Victor Almeida <victor.almeida@tfgco.com>
2018-01-12 17:30:50 -0200

    Trasparent distributed tracing using Jaeger

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+var eventContext = require('event-context');
+var eventContextPluginNode = require('event-context-plugin-node');
+
+eventContextPluginNode.patch();
+
 if(process.env.POMELO_RPC_COV) {
   module.exports.client = require('./lib-cov/rpc-client/client');
   module.exports.server = require('./lib-cov/rpc-server/server');
+  module.exports.tracer = require('./lib-cov/jaeger/tracer');
 } else {
   module.exports.client = require('./lib/rpc-client/client');
   module.exports.server = require('./lib/rpc-server/server');
+  module.exports.tracer = require('./lib/jaeger/tracer');
 }
