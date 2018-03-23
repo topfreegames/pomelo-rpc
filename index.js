57cc43991f5c064c012b7ac7d8fd04a26806bc2b
Victor Almeida <victor.almeida@tfgco.com>
2018-03-23 11:57:35 -0300

    Extraced Jaeger to its own module

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 if(process.env.POMELO_RPC_COV) {
   module.exports.client = require('./lib-cov/rpc-client/client');
   module.exports.server = require('./lib-cov/rpc-server/server');
-  module.exports.tracer = require('./lib-cov/jaeger/tracer');
 } else {
   module.exports.client = require('./lib/rpc-client/client');
   module.exports.server = require('./lib/rpc-server/server');
-  module.exports.tracer = require('./lib/jaeger/tracer');
 }
